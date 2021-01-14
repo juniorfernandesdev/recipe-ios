1ce3a0970c3896cc19203d51947f6cb992cc5eee
Junior Fernandes <juniorbass8@gmail.com>
2021-01-13 22:33:09 -0300

    Adicionando a pasta /POD no gitignore

---
+++ recipe/Network/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  recipe
+//
+//  Created by Junior Fernandes on 13/01/21.
+//
+
+import Foundation
---
+++ recipe/Network/Network.swift
@@ -0,0 +1,8 @@
+//
+//  Network.swift
+//  recipe
+//
+//  Created by Junior Fernandes on 13/01/21.
+//
+
+import Foundation
